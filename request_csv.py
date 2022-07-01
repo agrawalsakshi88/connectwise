aa4511c6f3f77044ad81fa6d448011bc476e2a29
Sakshi Agrawal <agrawalsakshi1850@gmail.com>
2022-07-01 19:05:11 +0530

    assignment

---
+++ request_csv.py
@@ -0,0 +1,21 @@
+import requests, sys
+
+if len(sys.argv) != 2:
+    print("Uses:\npython request_csv.py [FILE]")
+    sys.exit(1)
+    raise
+
+file=sys.argv[1]
+file_content=[]
+with open(file) as content:
+    for line in content:
+        try:
+            line=line.replace("\n","")
+            request=requests.get(line)
+            file_content.append(f"{line},{request.status_code},{request.elapsed.total_seconds()}\n")
+        except Exception as a:
+            print(a)
+        
+
+with open("request_result.csv", "w") as output:
+    output.writelines(file_content)
